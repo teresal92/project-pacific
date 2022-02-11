04bbd53d7d74530f516615fabe2657f94228fbe2
Estevan Gonzalez <estevangonzalez80@gmail.com>
2022-02-11 11:21:54 -0800

    Updated

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ProductDetailOverview from './ProductDetailOverview/ProductDetailOverview.jsx';
-import ProductList from './ProductDetailOverview/ProductListEntry.jsx';
 import QnACore from './Q&A/QnACore.jsx'
 
 import ProductList from './ProductList.jsx';
-import QnACore from './Q&A/QnACore.jsx';
 import exProductData from './exProductData.js';
 
 class App extends React.Component{
@@ -26,9 +24,6 @@ class App extends React.Component{
   render() {
     return(
       <div>
-        <div></div>
-        <ProductList getSelectedProduct={this.getSelectedProduct} />
-        {/* <ProductList getSelectedProduct={this.getSelectedProduct} /> */}
         <ProductList />
         <ProductDetailOverview product={this.state.selectedProduct} />
         <QnACore />
