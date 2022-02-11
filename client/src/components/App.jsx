c59a3efa885c1cc16636c6ba80c33f3c407f5732
tristenurban <tristen.urban@gmail.com>
2022-02-11 10:57:59 -0800

    Added config.js file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import QnACore from './Q&A/QnACore.jsx'
+import QnACore from './Q&A/QnACore.jsx';
+import ProductList from './ProductList.jsx'
+
 class App extends React.Component{
   constructor(props){
     super(props)
