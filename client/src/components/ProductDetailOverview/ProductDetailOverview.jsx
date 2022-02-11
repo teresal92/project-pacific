776ce6bf16351819a0acf822de8ae57030e8b377
Teresa Lew <teresalew92@gmail.com>
2022-02-10 22:08:07 -0800

    Refactored product info components using MUI

--- client/src/components/ProductDetailOverview/ProductDetailOverview.jsx
+++ client/src/components/ProductDetailOverview/ProductDetailOverview.jsx
@@ -4,12 +4,22 @@ import ImageGallery from './ImageGallery.jsx';
 import StyleSelector from './StyleSelector.jsx';
 import AddToCart from './AddToCart.jsx';
 import exStyleData from './exStyleData.js';
+import { API_KEY } from '../../config/config.js';
+// MUI
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+// Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faPinterest, faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import { API_KEY } from '../../config/config.js';
+
 const axios = require('axios');
 
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+}));
 
 function ProductDetailOverview({product}) {
   const [ productInfo, setProductInfo ] = useState([]);
@@ -43,26 +53,30 @@ function ProductDetailOverview({product}) {
 
   }, []);
 
-
-
   return (
-    <div>
-      <div className="col-left">
-        <ImageGallery styles={styles} />
-      </div>
-      <div className="col-right">
-        <span className='reviews'><a href="#">Read all 'REPLACE' reviews</a></span>
-        <span className='social'>
-          <FontAwesomeIcon icon={faHeart} />
-          <FontAwesomeIcon icon={faTwitter} />
-          <FontAwesomeIcon icon={faPinterest} />
-          <FontAwesomeIcon icon={faFacebook} />
-        </span>
-        <ProductInfo product={productInfo} style={selectedStyle} />
-        <StyleSelector styles={styles}/>
-        <AddToCart style={selectedStyle}/>
-      </div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Item>
+            <ImageGallery styles={styles} />
+          </Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>
+            <span className='reviews'><a href="#">Read all 'REPLACE' reviews</a></span>
+            <span className='social'>
+              <FontAwesomeIcon icon={faHeart} />
+              <FontAwesomeIcon icon={faTwitter} />
+              <FontAwesomeIcon icon={faPinterest} />
+              <FontAwesomeIcon icon={faFacebook} />
+            </span>
+            <ProductInfo product={productInfo} style={selectedStyle} />
+            <StyleSelector styles={styles}/>
+            <AddToCart style={selectedStyle}/>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
   )
 };
 
