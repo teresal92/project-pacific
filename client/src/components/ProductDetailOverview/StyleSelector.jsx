8177b77bf392568f114907bcc38d7e706e47c623
Teresa Lew <teresalew92@gmail.com>
2022-02-11 18:04:06 -0800

    Implemented checkbox overlay to conditionally render over selected thumbnail

--- client/src/components/ProductDetailOverview/StyleSelector.jsx
+++ client/src/components/ProductDetailOverview/StyleSelector.jsx
@@ -4,14 +4,30 @@ import React, { useState, useEffect } from 'react';
 import { ImageList, ImageListItem, ImageListItemBar, Typography } from '@mui/material';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
+
+// TODO:
+  // Conditionally render overlay checkbox on top right of thumbnail when style is selected
+
 function StyleSelector({styles, handleStyleSelector}) {
   const [ selectedStyle, setSelectedStyle ] = useState([]);
+  const [ isSelected, setIsSelected ] = useState(false);
   // after refactoring to pass down styles, should be able to access default
   // const [ selectedStyle, setSelectedStyle ] = useState(styles[0]);
 
-  const handleChange = (event, style) => {
+  const handleClick = (event, style) => {
     setSelectedStyle(style);
     handleStyleSelector(style);
+    setIsSelected(!isSelected);
+  };
+
+  const showSelected = (style) => {
+    if (isSelected && style.style_id === selectedStyle.style_id) {
+      return (
+        <div className='overlay'>
+          <CheckCircleOutlineIcon />
+        </div>
+      )
+    }
   };
 
   return (
@@ -21,7 +37,7 @@ function StyleSelector({styles, handleStyleSelector}) {
       </Typography>
       <ImageList sx={{ width: 400, height: 250 }} cols={4} rowHeight={100}>
         {styles.map((style) =>
-          <ImageListItem key={`style-id-${style.style_id}`} onClick={(e) => handleChange(e, style)}>
+          <ImageListItem key={`style-id-${style.style_id}`} onClick={(e) => handleClick(e, style)}>
             <img
               className='thumbnail'
               name={style.name}
@@ -30,6 +46,7 @@ function StyleSelector({styles, handleStyleSelector}) {
               alt={style.name}
               loading='lazy'
             />
+            {showSelected(style)}
           </ImageListItem>
         )}
       </ImageList>
