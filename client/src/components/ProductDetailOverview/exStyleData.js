a94b2dc8b174e876c3897c17b0ead30952add757
Teresa Lew <teresalew92@gmail.com>
2022-02-11 17:39:56 -0800

    Added photo urls to dummy data

--- client/src/components/ProductDetailOverview/exStyleData.js
+++ client/src/components/ProductDetailOverview/exStyleData.js
@@ -1,5 +1,13 @@
 const styleData = {
-  style_id: 253620, name: 'Forest Green & Black', original_price: '140.00', sale_price: '100.00', 'default?': true
+  style_id: 253620,
+  name: 'Forest Green & Black',
+  original_price: '140.00',
+  sale_price: '100.00',
+  'default?': true,
+  photos: [{
+    thumbnail_url: "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+    url: "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+  }]
 }
 
 export default styleData;
