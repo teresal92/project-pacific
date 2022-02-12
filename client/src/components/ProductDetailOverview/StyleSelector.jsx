ca677ed28a86c68a2afc891bd7794a22b3026574
Teresa Lew <teresalew92@gmail.com>
2022-02-11 17:40:47 -0800

    Added click handler event to select style thumbnail and update state of selectedStyle and display selectedStyle name

--- client/src/components/ProductDetailOverview/StyleSelector.jsx
+++ client/src/components/ProductDetailOverview/StyleSelector.jsx
@@ -1,33 +1,41 @@
 import React, { useState, useEffect } from 'react';
-// import StyleSelectorEntry from './StyleSelectorEntry.jsx';
 
 // MUI
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Typography from '@mui/material/Typography';
+import { ImageList, ImageListItem, ImageListItemBar, Typography } from '@mui/material';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
-function StyleSelector({styles}) {
+function StyleSelector({styles, handleStyleSelector}) {
+  const [ selectedStyle, setSelectedStyle ] = useState([]);
+  // after refactoring to pass down styles, should be able to access default
+  // const [ selectedStyle, setSelectedStyle ] = useState(styles[0]);
+
+  const handleChange = (event, style) => {
+    setSelectedStyle(style);
+    handleStyleSelector(style);
+  };
 
   return (
     <div>
       <Typography variant="overline" display="block" >
-        STYLE > SELECTED STYLE
+        STYLE > {selectedStyle.name ? selectedStyle.name : 'SELECT STYLE'}
       </Typography>
-      <ImageList sx={{ width: 500, height: 250 }} cols={4} rowHeight={100}>
-        {styles.map((style) => (
-        <ImageListItem key={`style-id-${style.style_id}`}>
-          <img
-            src={`${style.photos[0].thumbnail_url}?w=100&h=100&fit=crop&auto=format`}
-            srcSet={`${style.photos[0].thumbnail_url}?w=100&h=100&fit=crop&auto=format&dpr=2 2x`}
-            alt={style.name}
-            loading='lazy'
-          />
-        </ImageListItem>
-        ))}
+      <ImageList sx={{ width: 400, height: 250 }} cols={4} rowHeight={100}>
+        {styles.map((style) =>
+          <ImageListItem key={`style-id-${style.style_id}`} onClick={(e) => handleChange(e, style)}>
+            <img
+              className='thumbnail'
+              name={style.name}
+              src={`${style.photos[0].thumbnail_url}?w=100&h=100&fit=crop&auto=format`}
+              srcSet={`${style.photos[0].thumbnail_url}?w=100&h=100&fit=crop&auto=format&dpr=2 2x`}
+              alt={style.name}
+              loading='lazy'
+            />
+          </ImageListItem>
+        )}
       </ImageList>
-    </div>
-  )
+      </div>
+  );
 }
 
 export default StyleSelector;
+
