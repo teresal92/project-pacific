d690192ff0c9c091597d617a554d22b8cde2d68c
Teresa Lew <teresalew92@gmail.com>
2022-02-14 10:54:21 -0800

    Resolved merge conflict

--- client/src/components/ProductDetailOverview/ProductDetailOverview.jsx
+++ client/src/components/ProductDetailOverview/ProductDetailOverview.jsx
@@ -8,7 +8,7 @@ import { API_KEY } from '../../config/config.js';
 // MUI
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+// import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 // Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -17,14 +17,14 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const axios = require('axios');
 
-const Item = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   padding: theme.spacing(1),
+// }));
 
 function ProductDetailOverview({product}) {
   const [ productInfo, setProductInfo ] = useState([]);
   const [ styles, setStyles ] = useState([]);
-  // need to update fake data with specific style obj
+  // need to update fake data with specific style obj onSelect
   const [ selectedStyle, setSelectedStyle ] = useState(exStyleData);
   const productId = product[0].id;
 
@@ -55,14 +55,13 @@ function ProductDetailOverview({product}) {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Item>
-            <ImageGallery styles={styles} />
-          </Item>
+      <Grid container spacing={3}>
+        {/* for xs screen take up 6 cols */}
+        <Grid item xs={12} sm={6}>
+          <ImageGallery styles={styles} />
         </Grid>
-        <Grid item xs={6}>
-          <Item>
+        <Grid item xs={12} sm={6}>
+          <div>
             <span className='reviews'><a href="#">Read all 'REPLACE' reviews</a></span>
             <span className='social'>
               <FontAwesomeIcon icon={faHeart} />
@@ -73,7 +72,7 @@ function ProductDetailOverview({product}) {
             <ProductInfo product={productInfo} style={selectedStyle} />
             <StyleSelector styles={styles}/>
             <AddToCart style={selectedStyle}/>
-          </Item>
+          </div>
         </Grid>
       </Grid>
     </Box>
