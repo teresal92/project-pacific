63cb6ea228fd012a52146e3d94d2d86b105c32b4
tristenurban <tristen.urban@gmail.com>
2022-02-15 10:15:20 -0800

    CSS fixes and card images implemented

--- client/src/components/Home/ProductListEntry.jsx
+++ client/src/components/Home/ProductListEntry.jsx
@@ -7,12 +7,30 @@ const axios = require('axios');
 
 
 export default function ProductListEntry(props) {
+  const [photo, setPhoto] = useState([]);
+
+  useEffect(() => {
+    async function getPhoto() {
+      try {
+        const res = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.product.id}/styles`, {
+          headers: { 'Authorization': `${API_KEY}` }
+        });
+        setPhoto(res.data.results[0].photos[0])
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getPhoto();
+  }, []);
 
   return (
     <div>
-      <Card className='Category' onClick={(event) => {props.getId(props.product.id)}}>
+      <Card className='Category' onClick={(event) => { props.getId(props.product.id) }}>
         <CardHeader title={props.product.name} />
-        <CardContent />
+        <CardMedia
+        component='img'
+        height='300'
+        src={photo.url}/>
       </Card>
     </div>
   )
