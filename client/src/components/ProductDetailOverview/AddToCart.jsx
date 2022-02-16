a5bc0392fb3a1feefc45bc6f26cae561e4a05eaf
Teresa Lew <teresalew92@gmail.com>
2022-02-15 21:34:29 -0800

    Refactored size, qty, add to cart to MUI and set dropdown to conditionally open

--- client/src/components/ProductDetailOverview/AddToCart.jsx
+++ client/src/components/ProductDetailOverview/AddToCart.jsx
@@ -5,115 +5,200 @@ const axios = require('axios');
 // MUI components
 import Button from '@mui/material/Button';
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
-// import Box from '@mui/material/Box';
-// import InputLabel from '@mui/material/InputLabel';
-// import MenuItem from '@mui/material/MenuItem';
-// import FormControl from '@mui/material/FormControl';
-// import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function AddToCart({style}) {
-  const skus = style.skus
-  const skuIds = Object.keys(skus);
-  const skuList = skuIds.map(skuId => ({ id: skuId, quantity: skus[skuId].quantity, size: skus[skuId].size }));
+  const skus = style.skus;
+  const skuList = Object.keys(skus).map(skuId => ({ id: skuId, quantity: skus[skuId].quantity, size: skus[skuId].size }));
   // console.log(`skuList: ${JSON.stringify(skuList)}`);
-
   const [sku, setSku] = React.useState('');
   const [size, setSize] = React.useState('');
   const [qty, setQty] = React.useState('');
-  const [isValid, setIsValid] = React.useState(false);
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [OOS, setOOS] = React.useState(false);
 
-  /* Size Dropdown
-   * only list sizes that are currently instock for style selected
-   * if no remaining stock, dropdown should become inactive and read 'OUT OF STOCK'
-   * when collapsed, dropdown should show only currently selected size
-   * by default: dropdown should show 'Select Size'
-   */
-  // TODO: why is sku working in select value instead of size?
+  /* Size Dropdown */
   const renderSizeDropdown = () => {
     let availableSkus = skuList.filter(sku => sku.quantity > 0);
+    if (availableSkus.length === 0) {
+      setOOS(true);
+    }
 
-    return ((availableSkus.length > 0) ? (
-      <select className="size-dropdown" value={sku} onChange={handleSizeChange}>
-        <option value='' disabled>Select Size</option>
-        {availableSkus.map((sku, i) => <option key={i} value={sku.id}>{sku.size}</option>)}
-      </select>
-    ) : (
-      <select value={''} onChange={handleSizeChange} disabled>{'OUT OF STOCK'}</select>
-    ))
+    if (!OOS) {
+      return (
+        <FormControl sx={{ m: 1, minWidth: 140 }}>
+          <InputLabel id="demo-simple-select-label">Select Size</InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            open={open}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            value={size}
+            onChange={handleSizeChange}
+          >
+            <MenuItem value="" disabled>
+              <em>Select Size</em>
+            </MenuItem>
+            {availableSkus.map((sku, i) => (
+              <MenuItem
+                key={i}
+                value={sku.size}
+                onClick={(e) => handleSizeClick(e, sku)}
+              >
+                {sku.size}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      )
+    } else {
+      return (
+        <FormControl sx={{ m: 1, minWidth: 140 }}>
+          <InputLabel id="demo-simple-select-label">Select Size</InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            value={size}
+            onChange={handleSizeChange}
+          >
+            <MenuItem disabled value="">
+              <em>OOS</em>
+            </MenuItem>
+          </Select>
+        </FormControl>
+      )
+    }
+
+    // if (availableSkus.length > 0) {
+    //   return (
+    //     <select className="size-dropdown" value={sku} onChange={handleSizeChange}>
+    //       <option value='' disabled>Select Size</option>
+    //       {availableSkus.map((sku, i) => <option key={i} value={sku.id}>{sku.size}</option>)}
+    //     </select>
+    //   )
+    // } else {
+    //   setOOS(true);
+    //   return (
+    //     <select value={''} onChange={handleSizeChange} disabled>{'OUT OF STOCK'}</select>
+    //   )
+    // }
   };
 
-  /* Size Dropdown
-   * options: list range from 1-max.
-   * max is either qty of sku in stock or hard limit of 15
-   * when collapsed, dropdown should show only currently selected size
-   * by default: if size not selected, show '-' and dropdown will be disabled
-   * if size is selected, dropdown should default to 1
-   */
+  /* Size Dropdown */
   const renderQtyDropdown = () => {
-    if (sku) {
-      let max = (skus[sku].quantity < 15) ? skus[sku].quantity : 15;
+    if (sku !== '') {
+      console.log(sku.quantity);
+      let max = ( sku.quantity < 15) ? sku.quantity : 15;
       let range = [];
       for (let i = 1; i <= max; i++) {
         range.push(i);
       };
-      console.log(`range ${range}`);
-
       return (
-        <select value={qty} onChange={handleQtyChange}>
-          {range.map((num, i) => <option key={i} value={num}>{num}</option>)}
-        </select>
+        <FormControl sx={{ m: 1, minWidth: 140 }}>
+          <InputLabel id="demo-simple-select-label">Select Qty</InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            value={qty}
+            onChange={handleQtyChange}
+          >
+            {range.map((num, i) => (
+            <MenuItem
+              key={i}
+              value={num}
+            >
+              {num}
+            </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       )
     } else {
       return (
-        <select value={qty} onChange={handleQtyChange} disabled>
-          <option value=''>-</option>
-        </select>
+        <FormControl sx={{ m: 1, minWidth: 140 }}>
+          <InputLabel id="demo-simple-select-label">Select Qty</InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            value={qty}
+            onChange={handleQtyChange}
+          >
+            <MenuItem disabled value="">
+              <em>-</em>
+            </MenuItem>
+          </Select>
+        </FormControl>
       )
     }
+    // return (
+    //     <select value={qty} onChange={handleQtyChange}>
+    //       {range.map((num, i) => <option key={i} value={num}>{num}</option>)}
+    //     </select>
+    //   )
+    // } else {
+    //   return (
+    //     <select value={qty} onChange={handleQtyChange} disabled>
+    //       <option value=''>-</option>
+    //     </select>
+    //   )
+    // }
   };
 
-  /* Add to Cart Button
-   * if default 'Select Size' is currently selected: onClick should open size dropdown and message should appear above dropdown
-   * stating 'Please select size'
-   * if no stock: button should be hidden
-   * if both valid size and valid qty are valid, onClick will add product to user's cart
-   */
+  /* Add to Cart Button */
   const renderCartButton = () => {
-    return ( (size !== '') ?
+    return ( !OOS ?
     <div>
-      <Button onClick={handleButtonClick} variant="contained" startIcon={<ShoppingCartRoundedIcon />}>Add to Cart</Button>
+      <Button
+        onClick={handleButtonClick}
+        variant="contained"
+        startIcon={<ShoppingCartRoundedIcon />}
+      >
+        Add to Cart
+      </Button>
     </div> : null)
     // return ( (size !== '') ? <input type="submit" value="Add to Cart" onClick={handleButtonClick} /> : null)
   }
 
   // set state for sku and size based on sku id, then pass sku id to showQtyDropdown
   const handleSizeChange = (e) => {
-    let id = e.target.value;
-    let size = skus[id].size;
+    let size = e.target.value;
     setSize(size);
-    setSku(id);
   };
+
+  const handleSizeClick = (e, sku) => {
+    console.log(sku);
+    setSku(sku);
+  }
 
   const handleQtyChange = (e) => {
     setQty(e.target.value);
   };
 
-  /* On Submit of add to cart button
-   * add sku to cart by posting to '/cart' with sku_id as param
-   */
+  /* On Submit of add to cart button */
   const handleButtonClick = (e) => {
     e.preventDefault();
-    console.log('clicked!', e);
-    if (sku && size && qty) {
-      setIsValid(true);
-      // axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/cart', { 'sku_id': sku }, { headers: { 'Authorization': `${API_KEY}` } })
-      // .then( res => console.log(`Success! ${res}`))
-      // .catch( err => console.error(err));
+    if (sku && qty) {
+      setOpen(false);
+      axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/cart', { 'sku_id': sku.id }, { headers: { 'Authorization': `${API_KEY}` } })
+      .then( res => console.log(`Success! ${res}`))
+      .catch( err => console.error(err));
     } else {
-      setIsOpen(true);
+      setOpen(true);
     }
   };
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   return (
     <form className="add-to-cart-form">
@@ -123,44 +208,3 @@ export default function AddToCart({style}) {
     </form>
   )
 };
-
-// MUI form
-// <div>
-//   <FormControl sx={{ m: 1, minWidth: 120 }} >
-//     <InputLabel id="demo-simple-select-label">
-//       Select Size
-//     </InputLabel>
-//     <Select
-//       labelId="demo-simple-select-label"
-//       id="demo-simple-select"
-//       name="size"
-//       value={size}
-//       label="Size"
-//       onChange={handleSizeChange}
-//     >
-//       <MenuItem value=''><em>Select Size</em></MenuItem>
-//       {skus.map((sku, i) => <MenuItem key={`size-${i}`} value={sku.size}>{sku.size}</MenuItem>)}
-//     </Select>
-//   </FormControl>
-//   <FormControl sx={{ m: 1, minWidth: 120 }}>
-//     <InputLabel id="demo-simple-select-label">
-//       Select Qty
-//     </InputLabel>
-//     <Select
-//       labelId="demo-simple-select-label"
-//       id="demo-simple-select"
-//       name="size"
-//       value={qty}
-//       label="Qty"
-//       onChange={handleQtyChange}
-//     >
-//       <MenuItem value=''><em>Select Qty</em></MenuItem>
-//       {skus.map((sku, i) => <MenuItem key={`qty-${i}`} value={sku.quantity}>{sku.quantity}</MenuItem>)}
-//     </Select>
-//   </FormControl>
-//   <div>
-//     <Button variant="contained" startIcon={<ShoppingCartRoundedIcon />}>
-//       Add to Cart
-//     </Button>
-//   </div>
-// </div>
