222e7a33487e868dc641c25d2452520776b905b8
Estevan Gonzalez <estevangonzalez80@gmail.com>
2022-02-17 10:35:41 -0800

    Pull request up to date

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,12 +3,9 @@ import ProductDetailOverview from './ProductDetailOverview/ProductDetailOverview
 import ProductList from './Home/ProductList.jsx';
 import NavBar from './Home/NavBar.jsx';
 import QnACore from './Q&A/QnACore.jsx'
-<<<<<<< HEAD
 import { Divider } from '@mui/material';
-=======
 import OutfitList from './OutfitnRelated/YourOutfit.jsx'
 
->>>>>>> Added OutfitnRelated folder with related items and youroutfit list
 import exProductData from './exProductData.js';
 
 class App extends React.Component {
@@ -45,7 +42,6 @@ class App extends React.Component {
           </div>
           <div>
             <ProductDetailOverview productId={this.state.selectedProduct.id} />
-<<<<<<< HEAD
             <Divider
             sx={{
               mt: 10,
@@ -53,14 +49,12 @@ class App extends React.Component {
             }}
             varient='middle'/>
             <QnACore prodId={this.state.selectedProduct.id}/>
-=======
           </div>
           <div className='outfitList'>
             <OutfitList outfit={this.state.outfit} selected={this.state.productSelected} />
           </div>
           <div className='qna'>
             <QnACore prodId={this.state.selectedProduct.id} />
->>>>>>> Added OutfitnRelated folder with related items and youroutfit list
           </div>
         </div >
       ) : (
