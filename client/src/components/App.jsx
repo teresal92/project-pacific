853b0710e251aa3cf20740b373fd620347fdb277
tristenurban <tristen.urban@gmail.com>
2022-02-18 09:39:04 -0800

    Initialized changes for OutfitList to be a child component of ProductDetailOverview

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,11 @@ import ProductList from './Home/ProductList.jsx';
 import NavBar from './Home/NavBar.jsx';
 import QnACore from './Q&A/QnACore.jsx'
 import { Divider } from '@mui/material';
+<<<<<<< HEAD
 import OutfitList from './OutfitnRelated/YourOutfit.jsx'
+=======
+import OutfitList from './OutfitnRelated/YourOutfit.jsx';
+>>>>>>> Initialized changes for OutfitList to be a child component of ProductDetailOverview
 
 import exProductData from './exProductData.js';
 
@@ -14,7 +18,7 @@ class App extends React.Component {
     this.state = {
       selectedProduct: exProductData[0],
       productSelected: false,
-      outfit: exProductData,
+      outfit: [],
     }
   }
 
@@ -33,6 +37,13 @@ class App extends React.Component {
     })
   }
 
+  addToOutfit = (event) => {
+    // console.log(event)
+    this.setState({
+      outfit: this.state.outfit.concat(event)
+    })
+  }
+
   render() {
     return (
       this.state.productSelected ? (
@@ -41,18 +52,32 @@ class App extends React.Component {
             <NavBar categories={this.getHome.bind(this)} />
           </div>
           <div>
+<<<<<<< HEAD
             <ProductDetailOverview productId={this.state.selectedProduct.id} />
+=======
+            <ProductDetailOverview
+            productId={this.state.selectedProduct.id}
+            outfit={this.state.outfit}
+            selected={this.state.productSelected}
+            add={this.addToOutfit.bind(this)}/>
+>>>>>>> Initialized changes for OutfitList to be a child component of ProductDetailOverview
             <Divider
             sx={{
               mt: 10,
               mx: 40,
             }}
             varient='middle'/>
+<<<<<<< HEAD
 <
             <QnACore prodId={this.state.selectedProduct.id}/>
           </div>
           <div className='qna'>
 
+=======
+          </div>
+          <div className='qna'>
+            <QnACore prodId={this.state.selectedProduct.id} />
+>>>>>>> Initialized changes for OutfitList to be a child component of ProductDetailOverview
           </div>
         </div >
       ) : (
@@ -64,8 +89,18 @@ class App extends React.Component {
             <ProductList getId={this.getProductId.bind(this)} />
             <div className='textbox'>text box</div>
           </div>
+          <div className='homeDivider'>
+          <Divider
+            sx={{
+              mt: 10,
+              mx: 40,
+            }}
+            varient='middle'/>
+          </div>
           <div className='outfitList'>
-            <OutfitList outfit={this.state.outfit} selected={this.state.productSelected} />
+            <OutfitList
+            outfit={this.state.outfit}
+            selected={this.state.productSelected}/>
           </div>
         </div>
       ))
